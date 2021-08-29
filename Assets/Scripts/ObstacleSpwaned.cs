d9d53df18338bdbd975b08eeb6d7caf58862750a
RobertyLive <roberttylive@gmail.com>
2021-08-28 22:07:55 -0300

    Create Obstacle moviment in game

--- Assets/Scripts/ObstacleSpwaned.cs
+++ Assets/Scripts/ObstacleSpwaned.cs
@@ -17,13 +17,35 @@ public class ObstacleSpwaned : MonoBehaviour
     {
         spawnPos = transform.position;
 
-        InvokeRepeating(nameof(Spawn), 1f, spawnRate);
+        //InvokeRepeating(nameof(Spawn), 1f, spawnRate);
+        StartCoroutine(nameof(SpawnObstacle));
     }
 
+    IEnumerator SpawnObstacle()
+    {
+        while(true)
+        {
+            Spawn();
+
+            yield return new WaitForSeconds(spawnRate);
+        }
+    }
     private void Spawn()
     {
         int randObstacle = Random.Range(0, obstacle.Length);
 
+        if(randObstacle == 2)
+        {
+            spawnPos = new Vector3(10f, 0.5f, 0);
+
+            Invoke(nameof(TimeRate), 0.5f);
+        }
+
         Instantiate(obstacle[randObstacle], spawnPos, transform.rotation);
+    }
+
+    private void TimeRate()
+    {
+        spawnPos = transform.position;
     }
 }
